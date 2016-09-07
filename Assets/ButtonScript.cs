f4ebd19b505ee47622173c1c9c4b34ec5260aec1
DESKTOP-7OOGRRA\dkfan <dkfang7@gmail.com>
2016-09-07 13:17:05 -0400

    Added ui skeleton logic, todo: design some screens/views

---
+++ Assets/ButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+    public void quitGameOnClick()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+    public void levelSelectOnClick()
+    {
+
+    }
+    public void dictionaryOnClick()
+    {
+        GameObject dictionary = GameObject.FindGameObjectWithTag("Dictionary");
+        GameObject menu = GameObject.Find("MainMenu");
+
+        dictionary.SetActive(true);
+        menu.SetActive(false);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
