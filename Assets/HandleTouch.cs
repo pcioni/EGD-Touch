bf5c47f601f48cc769b95d922ed44c8e3a588c51
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2016-09-09 14:38:36 -0400

    Add animation to tiles - still need to set up game logic etc

---
+++ Assets/HandleTouch.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleTouch : MonoBehaviour {
+
+    public int rotationSpeed = 1;
+
+    bool isMatched = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void OnMouseDown() {
+        // animate flip
+        StartCoroutine(animateFlip());
+
+        // TODO: handle logic and play vibration
+    }
+
+    IEnumerator animateFlip() {
+		for (int i = 0; i < 180; i += rotationSpeed) {
+			gameObject.transform.Rotate (new Vector3 (0, rotationSpeed, 0));
+            yield return null;
+		}
+	}
+}
