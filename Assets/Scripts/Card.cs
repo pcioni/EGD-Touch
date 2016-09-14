aaa2f3cf842650d2402ecb9637d5d7a56759dcd8
DESKTOP-7OOGRRA\dkfan <dkfang7@gmail.com>
2016-09-14 12:53:44 -0400

    fixed mirror image

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -11,7 +11,8 @@ public class Card : MonoBehaviour {
         string assetPath = "MorseCode/" + letter;
         GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>(assetPath);
         GetComponent<BoxCollider2D>().size = new Vector2(1 / scale, 1 / scale);
-        transform.localScale = new Vector3(scale, scale);
+        transform.localScale = new Vector3(scale * -1, scale);
+      //  this.gameObject.transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.x, 180 * Mathf.Deg2Rad, transform.localRotation.z));
     }
 }
 
